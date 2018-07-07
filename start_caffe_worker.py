5d84fb571358850d55bb29911fa2cc00c12c3ea1
Taritree <taritree.wongjirad@gmail.com>
2018-07-06 22:40:43 -0400

    added ssh tunnel option for worker

--- start_caffe_worker.py
+++ start_caffe_worker.py
@@ -12,11 +12,18 @@ if __name__ == "__main__":
     parser.add_argument( "-g", "--gpuid", required=True, type=int, help="gpuid to run code")
     parser.add_argument( "-w", "--weight_dir", required=True, type=str, help="weight directory")
     parser.add_argument( "-m", "--model_dir", required=True, type=str, help="model directory")
+    parser.add_argument( "-s", "--ssh",       default="",    type=str, help="tunnel through server via ssh")
 
     print sys.argv
     args = parser.parse_args( sys.argv[1:] )
+
+    ssh = None
+    if args.ssh!="":
+        ssh = args.ssh
     
-    worker = CaffeLArCV1Worker( args.identity, args.broker, port=args.port, gpuid=args.gpuid, weight_dir=args.weight_dir, model_dir=args.model_dir )
+    worker = CaffeLArCV1Worker( args.identity, args.broker,
+                                port=args.port, gpuid=args.gpuid, weight_dir=args.weight_dir, model_dir=args.model_dir,
+                                ssh_thru_server=ssh )
     worker.do_work()
 
     print "Worker exiting"
