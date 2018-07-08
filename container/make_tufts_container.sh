a4ef4ac6a216622bb86a7170d9af9f101950bfdf
Taritree <taritree.wongjirad@gmail.com>
2018-07-08 00:14:03 -0400

    more bugs

--- container/make_tufts_container.sh
+++ container/make_tufts_container.sh
@@ -3,4 +3,4 @@
 imgname=singularity-ssnetserver-caffelarbys-cuda8.0.img
 
 rm -f $imgname
-sudo singularity build ${imgname} SingularityNoDirvers
+sudo singularity build ${imgname} SingularityNoDrivers
