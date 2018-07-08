eaabc10b7e2d6788fecc9df179bf94d1c8cc7535
Taritree <taritree.wongjirad@gmail.com>
2018-07-08 00:00:18 -0400

    save

--- container/setup_caffelarbys_container.sh
+++ container/setup_caffelarbys_container.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # SETUP ROOT
-source /usr/local/root/release/bin/thisroot.sh
+source /usr/local/bin/thisroot.sh
 
 # SETUP CAFFE
 export CAFFE_ROOT=/usr/local/larbys/ssnet_example/sw/caffe
 export CAFFE_LIBDIR=${CAFFE_ROOT}/build/lib
 export CAFFE_INCDIR=${CAFFE_ROOT}/build/include
 export CAFFE_BINDIR=${CAFFE_ROOT}/build/tools
-export CAFFE_PYTHON=${CAFFE_ROOT}/python
+export CAFFE_PYTHONDIR=${CAFFE_ROOT}/python
 
 # OPENCV
 export OPENCV_INCDIR=/usr/local/include
